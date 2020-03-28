33d9ba351c00acf823f73253ca634dec80f51035
Teo Temonen <teo.temonen@protonmail.com>
2020-03-28 16:22:09 +0200

    Derp fix

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,15 +1,10 @@
 import Bot from '../client/client';
 import { Client } from 'discord.js';
-import Bot from '../client/client';
 
 export = class ReadyEvent extends Bot {
 
   constructor(client: Client) {
-<<<<<<< HEAD
     super()
-=======
-    super();
->>>>>>> b347a15a1fdd1ca570f2097340a88ab07762c004
     this.client = client
   }
 
