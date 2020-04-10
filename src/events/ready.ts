6e206ca718804997184e6a22d8c2762524feb2cb
Teo Temonen <teo.temonen@protonmail.com>
2020-04-10 19:37:14 +0300

    Moved corona statistics to start with ready event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,9 @@
 import Bot from '../client/client';
 import { Client } from 'discord.js';
+import dayjs from "dayjs";
+
+import axios from "axios";
+axios.defaults;
 
 export = class ReadyEvent extends Bot {
 
@@ -16,6 +20,46 @@ export = class ReadyEvent extends Bot {
       console.log(link);
     }
     catch (e) {
+      console.error(e);
+    }
+
+    // Corona statistics activity
+    try {
+      const activity = async () => {
+      const a = await axios.get('https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData/v2');
+
+      const c = a.data.confirmed;
+      const d = a.data.deaths;
+
+      const estimate = (items: any[]) => {
+        const timeNow = dayjs();
+  
+        let counter = 0;
+        items.forEach(item => {
+          const timeInCase = dayjs(item.date);
+          const daysDifference = timeInCase.diff(timeNow, 'day');
+  
+          if (daysDifference < -13 ) counter++;
+          else return;
+        })
+  
+        return counter;
+      }
+  
+      const estimateRecovered = estimate(c) - d.length;
+
+      this.client.user?.setPresence({
+        activity: {
+          name: `C:${c.length}, D:${d.length}, R:${estimateRecovered}`,
+          type: 'WATCHING',
+          url: 'https://github.com/HS-Datadesk/koronavirus-avoindata'
+        }
+      })
+    }
+
+    activity();
+    setInterval(activity, 600000);
+    } catch (e) {
       console.error(e);
     }
   }
