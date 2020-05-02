c8e095479eec7b4f0074f25c1499a1877774cfda
Teo Temonen <teo.temonen@protonmail.com>
2020-05-02 08:02:33 +0300

    Added alias

--- src/commands/Audio/Play.ts
+++ src/commands/Audio/Play.ts
@@ -6,23 +6,24 @@ export default class Play extends Command {
   constructor (client: Client) {
     super(client, {
       name: 'play',
+      aliases: ['sr'],
       description: 'Plays audio stream',
-    })
+    });
   }
 
   public async execute(_client: Client, _message: Message, _args: [string]) {
     if (!_message.member?.voice.channel) return _message.channel.send('You have to be in a voice channel!');
 
     const guild = cache.get(_message.guild?.id);
-    
+
     if (!_args) return console.log('No args');
-    
+
     if (!guild) {
       cache.set(_message.guild?.id, new Dispatcher());
       const guild = cache.get(_message.guild?.id);
-      guild.play(_message, _args)
+      guild.play(_message, _args);
     } else {
-      guild.play(_message, _args)
+      guild.play(_message, _args);
     }
   }
 }
